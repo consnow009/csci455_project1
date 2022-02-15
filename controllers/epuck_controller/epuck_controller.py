7919724cc1f6da3a52203209a3ba611187cc6527
Connor Lowe <con.lowe406@gmail.com>
2022-02-15 15:39:52 -0700

    The e-fuck now has a touch sensor that doesn't really work worth a damn. Also the e-fuck just decides to flip randomly.

--- controllers/epuck_controller/epuck_controller.py
+++ controllers/epuck_controller/epuck_controller.py
@@ -9,7 +9,7 @@ def exec_robot(robot):
     # this is specified in the documentation
     max_speed = 6.28 / 2
 
-    # get the motor, enable, and set motor devices
+    # get the motors, enable, and set motor devices
     left_motor = robot.getDevice('left wheel motor')
     right_motor = robot.getDevice('right wheel motor')
 
@@ -26,6 +26,10 @@ def exec_robot(robot):
         sensors.append(robot.getDevice(sensor_name))
         sensors[i].enable(TIMESTEP)
 
+    # get and enable touch sensor
+    touch = robot.getDevice('touch sensor')
+    touch.enable(TIMESTEP)
+
     # main loop
     while robot.step(TIMESTEP) != -1:
         # print the sensor and its corresponding value
@@ -33,8 +37,8 @@ def exec_robot(robot):
             print("Sensor: {}, Value: {}".format(i, sensors[i].getValue()))
 
         # interpret prox sensor data
-        right_wall = sensors[2].getValue() > 80
-        front_wall = sensors[0].getValue() > 80
+        right_wall = sensors[2].getValue() > 120
+        front_wall = sensors[0].getValue() > 120
 
         # set initial speeds
         left_speed = max_speed
@@ -51,12 +55,17 @@ def exec_robot(robot):
                 right_speed = max_speed
             else:
                 print("Turn right")
-                left_speed = max_speed/2
+                left_speed = max_speed
                 right_speed = max_speed/4
+
+        if touch.getValue() > 0:
+            print("HIT")
+            break
 
         # move the e-fuck
         left_motor.setVelocity(left_speed)
         right_motor.setVelocity(right_speed)
+
 
 
 # Main
